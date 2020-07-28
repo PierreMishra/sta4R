d8c155b5628188a8b684e8983945edd14f86e27d
Pierre Mishra <prashank.mishra@duke.edu>
2020-07-28 17:05:26 -0400

    made location function

--- sta4r_pierre/practice.R
+++ sta4r_pierre/practice.R
@@ -9,39 +9,59 @@ get_things <- function(api, iot_id = NULL, name = NULL) {
   if (is.null(iot_id) & is.null(name)){
     x <- GET(url = paste0(api, "/Things"))
     df <- fromJSON(as.character(x))
-    return (df)
-  } else if (is.null(iot_id)) {
-    x <- GET (url = paste0(api, "/Things?$filter=name eq ", name))
-    df <- fromJSON(as.character(x))
-    return (df)
-  } else if (is.null(name)) {
-    x <- GET (url = paste0(api, "/Things?$filter=@iot.id eq ", iot_id))
-    df <- fromJSON(as.character(x))
-    return (df)
-  }
+    return (df$value)
+  } 
   
+  else if (is.null(iot_id)) {
+    x <- GET (url = paste0(api, "/Things?$filter=name%20eq%20%27", name, "%27"))
+    df <- fromJSON(as.character(x))
+    return (df$value)
+  } 
+  
+  else if (is.null(name)) {
+    x <- GET (url = paste0(api, "/Things?$filter=@iot.id%20eq%20", iot_id))
+    df <- fromJSON(as.character(x))
+    return (df$value)
+  }
 }
 
 #iot_id <- 2
 #name <- 'TV-157'
-
-abc <- get_things(api=api, iot_id = 2)
-
+#abc <- get_things(api=api, name = "TV-157")
 
 
-
-# get 
-
-things <- get_things(api = api)
-
-# get locations
-get_location <- function (api){
-  x <- GET(url = paste0(api, "/Locations"))
-  return (x)
+# get location (iot_id, location.coordinates)
+get_location <- function (api, iot_id=NULL, location.coordinates=NULL, location.type=NULL){
+  
+  if (is.null(iot_id) & is.null(location.coordinates) & is.null(location.type)){
+    x <- GET(url = paste0(api, "/Locations"))
+    df <- fromJSON(as.character(x))
+    return (df$value)
+  }
+  
+  else if (is.null(location.coordinates)){
+    x <- GET(url = paste0(api, "/Locations?$filter=@iot.id%20eq%20", iot_id))
+    df <- fromJSON(as.character(x))
+    return (df$value)
+  }
+  
+  else if (is.null(iot_id)){
+    x <- GET(url = paste0(api, "/Locations?$filter=st_equals(location,geography%27", 
+                          toupper(location.type), "(", paste(c(location.coordinates), collapse = "%20"), ")%27)" ))
+    df <- fromJSON(as.character(x))
+    return (df$value)
+  }
+  
+  
 }
+# type <- "point"
+# lc <- c(-105.795, 32.8542)
+# L <- get_location(api, location.coordinates = lc, location.type = type)
 
-L <- get_location(api)
-L
+# get datastreams
+
+
+
 
 #### Entities [$expand, $select]
 
