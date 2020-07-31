275b3ffbb51a311f46ec7b4475e4784e8c4e97a0
Pierre Mishra <prashank.mishra@duke.edu>
2020-07-31 18:30:45 -0400

    kyle's notes

--- sta4r_pierre/practice.R
+++ sta4r_pierre/practice.R
@@ -27,7 +27,7 @@ get_things <- function(api, iot_id = NULL, name = NULL) {
 
 #iot_id <- 2
 #name <- 'TV-157'
-#abc <- get_things(api=api, name = "TV-157")
+abc <- get_things(api=api,iot_id <- 2)
 
 
 # get location (iot_id, location.coordinates, name) description too ????
@@ -67,6 +67,9 @@ name <- "MBOWN-208 - 29S.02E.13.113A"
 # L <- get_location(api, location.coordinates = lc, location.type = type)
 abc <- get_locations(api=api, name=name)
 
+# query by general location area (doesnt have to be specific)
+# search by area, http://localhost/v1.0/Locations?$filter=st_intersects(location, geography'POLYGON ((-180 -90, -180 90, 180 90, 180 -90, -180 -90))') 
+
 # get datastreams
 get_datastreams <- function (api, iot_id=NULL, name=NULL){
   x <- GET(url = paste0(api, "/Datastreams"))
@@ -75,6 +78,10 @@ get_datastreams <- function (api, iot_id=NULL, name=NULL){
 }
 
 
+
+# they probably are interested in datastreeams, (by location, time, observed property)
+# look at sls funtions made by the other person, phenomenon is observedProperty, sites=locations
+# pagination
 
 
 #### Entities [$expand, $select]
